6e8939b7a3b52b3a9b1e9572581702fef3d1d68c
Martin Skočík <mskocik@gmail.com>
2025-03-22 12:06:22 +0100

    add controller & test stubs

---
+++ tests/Navigation/JobTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Navigation;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class JobTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testNavigation(string $url): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful(); 
+    }
+
+    public static function urlProvider(): array
+    {
+        return [
+            ['/'],
+            // TODO: detail page
+        ];
+    }
+}
